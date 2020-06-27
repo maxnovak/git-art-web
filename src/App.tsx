d56bf7db1ab7d250ebc4c91f6165d0d4d842c719
Maxwell Novak <novak.max@gmail.com>
2020-06-27 15:39:40 -0400

    Fix lint errors
    It appears that there is a typescript/parcel issue that has not been
    merged in yet - parcel-bundler/parcel#4307 once that is fixed the ignore
    should be removed and the actual type for the return placed in the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const App = () => (
   <React.Fragment>
     <h1>Page under construction</h1>
-    <p>Someday you'll be able to make requests to generate some git art from here</p>
+    <p>Someday you&apos;ll be able to make requests to generate some git art from here</p>
   </React.Fragment>
 );
