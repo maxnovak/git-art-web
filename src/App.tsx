d244a30b3eb429b5e483993ac02bc325f8735d4e
Max Novak <novak.max@gmail.com>
2020-06-27 15:50:12 -0400

    Remove boiler plate (#1)
    * Remove boiler plate info from readme and add relevant info
    * Add PR template
    * Clean up imports and exports
    * Update page copy to reflact what this site is doing
    * Add eslint to repo
    * Add eslint config and target in package.json
    * Make parcel a dependency since it is required for builds
    * Fix lint errors
    It appears that there is a typescript/parcel issue that has not been
    merged in yet - parcel-bundler/parcel#4307 once that is fixed the ignore
    should be removed and the actual type for the return placed in the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import * as React from "react";
+import React from "react";
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const App = () => (
+  <React.Fragment>
+    <h1>Page under construction</h1>
+    <p>Someday you&apos;ll be able to make requests to generate some git art from here</p>
+  </React.Fragment>
 );
