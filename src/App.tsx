7f8b2ea0dbb004f6061f17bee191b49e357aca5d
Maxwell Novak <novak.max@gmail.com>
2020-06-27 14:18:26 -0400

    Clean up imports and exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
+import React from "react";
 
-export default () => (
-  <>
+export const App = () => (
+  <React.Fragment>
     <h1>Welcome to React Parcel Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
-  </>
+  </React.Fragment>
 );
