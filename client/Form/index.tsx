647b311b6adf42d154515c6402a4a65fc940a149
Maxwell Novak <novak.max@gmail.com>
2020-07-06 17:41:01 -0400

    Update state for each field and log on click

--- client/Form/index.tsx
+++ client/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '../Button';
 import { TextField } from '../InputFields';
@@ -12,14 +12,30 @@ const InputContainer = styled.div`
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const Form = () => {
+  const [repoName, setRepoName] = useState('');
+  const [year, setYear] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [word, setWord] = useState('');
+
   return <React.Fragment>
     <InputContainer>
-      <TextField type="text" placeholder="Name of Repository" />
-      <TextField type="number" placeholder="Year" />
-      <TextField type="text" placeholder="Name" />
-      <TextField type="email" placeholder="Email" />
-      <TextField type="text" placeholder="Word to Draw" />
+      <TextField type="text" placeholder="Name of Repository"
+        onChange={ event => setRepoName(event.target.value) }
+        />
+      <TextField type="number" placeholder="Year"
+        onChange={ event => setYear(event.target.value) }
+        />
+      <TextField type="text" placeholder="Name"
+        onChange={ event => setName(event.target.value) }
+        />
+      <TextField type="email" placeholder="Email"
+        onChange={ event => setEmail(event.target.value) }
+        />
+      <TextField type="text" placeholder="Word to Draw"
+        onChange={ event => setWord(event.target.value) }
+        />
     </InputContainer>
-    <Button>Create Art</Button>
+    <Button onClick={() => (console.log(repoName, year, name, email, word))}>Create Art</Button>
   </React.Fragment>
 }
