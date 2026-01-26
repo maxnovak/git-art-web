fa44fb0990324902b167f747f62b9e92bd1267fc
Maxwell Novak <novak.max@gmail.com>
2026-01-26 10:58:41 -0500

    Use env production for deploy

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -33,7 +33,7 @@ export const Form = () => {
       body,
     };
     try {
-      const response = await fetch(`http://${import.meta.env.VITE_GIT_ART_API}/make-art`, requestOptions)
+      const response = await fetch(`${import.meta.env.VITE_GIT_ART_API}/make-art`, requestOptions)
       const data = await response.blob();
       const file = await window.URL.createObjectURL(data);
       window.location.assign(file);
